a23480420e87fc2152e89832beaffc78c59db3cb
Weston <D.Weston@bearfoot.design>
2018-10-09 12:50:03 +1000

    Added Navigation

---
+++ bragginBowl/bragginBowl/bragginBowl/MasterPageItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bragginBowl
+{
+    class MasterPageItem
+    {
+        public string Title { get; set; }
+        public Type TargetType { get; set; }
+
+        public MasterPageItem()
+        {
+
+        }
+    }
+}
